1cee278b7ff7df5200b661dae006c196bcacef1d
ramilqO <osmanovramil823@gmail.com>
2021-11-20 21:16:04 +0600

    first

---
+++ js/cart.js
@@ -0,0 +1,17 @@
+const cart = () => {
+const cartBtn = document.querySelector(".button-cart");//кнопка открытия модалки
+const cartModal = document.getElementById('modal-cart');//модалка
+const modalClose = document.querySelector('.modal-close');//закрытие модалки
+
+function openModal() {
+    cartModal.style.display = "flex";
+}
+function closeModal() {
+    cartModal.style.display = "none";
+}
+
+cartBtn.addEventListener("click", () => {openModal()});
+modalClose.addEventListener("click", () => { closeModal()});
+}
+
+cart()
